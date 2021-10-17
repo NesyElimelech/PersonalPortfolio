7e12fee7419662f68328a576d4dc056f5d530621
NesyElimelech <nesy15@gmail.com>
2021-10-17 20:41:39 +0300

    Projects section - add more descripions for some projects.

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: 'Crwn-Clothing - E-Commerce',
     description:
-      "While building it I've learn many advanced React topics such as routing, Redux for state management, fetching data with Axios, as well as how to use Stripe for card transactions. On top of that, I've learn how to use Firebase's authentication and real time database, how to use Local Storage to save a current state and How to build a full functional server built with Node.js with API requests for data from the database using RESFull routes and CRUD Pattern.",
+      "While building it I've learn many advanced React topics such as routing, Redux for state management, fetching data with Axios, as well as how to use Stripe for card transactions. On top of that, I've learn how to use Firebase's authentication and real time database, how to use Local Storage to save a current state and How to build a full functional server built with Node.js with API requests for data fetching from the database using RESFull routes and CRUD Pattern.",
     image: '/images/Crwn.png',
     tags: ['React', 'Node.js', 'Express', 'Firebase', 'Stripe API'],
     source: 'https://github.com/NesyElimelech/crwn-clothing',
@@ -12,62 +12,12 @@ export const projects = [
   {
     title: 'Natours',
     description:
-      "My first All SCSS, HTML project. In this project I've learn how to use SCSS, how to make CSS animations, how to mimic JS functionality with only SCSS and HTML. This website is a landing page for a touring company with pricing card and testimonials from other users.",
+      "This website is a landing page for a touring company with pricing card and testimonials from other users. This is my first All SCSS, HTML project. In this project I've learn how to use SCSS, how to make CSS animations, how to mimic JS functionality with only SCSS and HTML. I've also learn how to use BEM pattern in Scss",
     image: '/images/Natuors.png',
     tags: ['HTML', 'SCSS'],
     visit: 'https://natour-lyart.vercel.app/',
     source: 'https://github.com/NesyElimelech/Natours-scss-advanced',
     id: 1,
-  },
-  {
-    title: 'Disney Plus Clone',
-    description:
-      "This is a Disney Plus website clone, I've use React, React Router, React Context API and Styled Components for the Front-End. And used Firebase's realtime database and authentication for the Back-End. It's built with 3 pages: Welcome page, Home page and a Movie/Series page all the data is fetched from the firebase database.",
-    image: '/images/DisneyPlus.png',
-    tags: ['React', 'Styled-Components', 'Firebase'],
-    source: 'https://github.com/NesyElimelech/disney_plus_clone',
-    visit: 'https://disney-plus-clone-hazel.vercel.app/home',
-    id: 2,
-  },
-  {
-    title: 'Infinite Scrolling - UnSplash API',
-    description:
-      "This is a pure vanilla JavaScript project that I've build with the UnSplash API for fetching a specific amount of photos and every time the user is scrolling down it's keep fetching for infinite loop which give the web app an infinite scrolling effect. In this project I've learn how to use ASync/Await functionality to make requests for the data from the UnSplash API and represent it.",
-    image: '/images/InfiniteScroll.png',
-    tags: ['JavaScript', 'UnSplash API'],
-    source: 'https://github.com/NesyElimelech/Unsplash-API-Infinite-Scrolling',
-    visit: 'https://unsplash-api-infinite-scrolling.vercel.app/',
-    id: 3,
-  },
-  {
-    title: 'React Music Player',
-    description:
-      "This is a React Music Player Web-App, In this project I've learned how to use Styled-Components in React projects for the first time. I've also built all the functionality and logic of a music player. This project is a part of a React course that I took by DevEd.",
-    image: '/images/ReactMusicPlayer.png',
-    tags: ['React', 'Styled-Components'],
-    source: 'https://google.com',
-    visit: 'https://react-music-player-ochre.vercel.app/',
-    id: 4,
-  },
-  {
-    title: 'Capture Portfolio',
-    description:
-      "This is another React project which was a part of DevEd React course, This time I've build a portfolio website for a photographer, All the animations in this website are made with Framer-Motion.",
-    image: '/images/Capture.png',
-    tags: ['React', 'Framer-Motion', 'Styled-Components'],
-    source: 'https://github.com/NesyElimelech/Capture-Portfolio',
-    visit: 'https://capture-portfolio-two.vercel.app/',
-    id: 5,
-  },
-  {
-    title: 'Countdown',
-    description:
-      'Nice little JavaScript Project that counts down to something the user entered. It does all the remaining time calculations until the countdown is complete. This project was purely built with JavaScript and HTML.',
-    image: '/images/Countdown.png',
-    tags: ['Javascript', 'HTML', 'CSS'],
-    source: 'https://github.com/NesyElimelech/Final-Countdown',
-    visit: 'https://final-countdown-one.vercel.app/',
-    id: 6,
   },
   {
     title: 'Yelp-Camp',
@@ -77,12 +27,62 @@ export const projects = [
     tags: ['Bootstrap', 'HTML', 'CSS', 'MongoDB', 'JavaScript'],
     source: 'https://github.com/NesyElimelech/Final-Countdown',
     visit: 'https://yelpcampnesy.herokuapp.com/',
+    id: 2,
+  },
+  {
+    title: 'Infinite Scrolling - UnSplash API',
+    description:
+      "This is a pure vanilla JavaScript project that I built with the UnSplash API for fetching a specific amount of photos and every time the user is scrolling down it's keep fetching for infinite loop which give the web app a feel of infinite scrolling effect. In this project I've learn how to use ASync/Await functionality to make requests for the data from the UnSplash API and represent it.",
+    image: '/images/InfiniteScroll.png',
+    tags: ['JavaScript', 'UnSplash API'],
+    source: 'https://github.com/NesyElimelech/Unsplash-API-Infinite-Scrolling',
+    visit: 'https://unsplash-api-infinite-scrolling.vercel.app/',
+    id: 3,
+  },
+  {
+    title: 'React Music Player',
+    description:
+      "This is a React Music Player Web-App, In this project I've learned how to use Styled-Components in React projects for the first time. I've also built all the functionality and logic of a music player such as play/pause and forward buttons and a library of songs that you can shuffle through. This project is a part of a React course that I took by DevEd.",
+    image: '/images/ReactMusicPlayer.png',
+    tags: ['React', 'Styled-Components'],
+    source: 'https://google.com',
+    visit: 'https://react-music-player-ochre.vercel.app/',
+    id: 4,
+  },
+  {
+    title: 'Capture Portfolio',
+    description:
+      "This is a film company portfolio website. built with React and Styled-Components, All the animations in this website are made with Framer-Motion. I've used React Router to navigate between the pages.",
+    image: '/images/Capture.png',
+    tags: ['React', 'Framer-Motion', 'Styled-Components', 'React-Router'],
+    source: 'https://github.com/NesyElimelech/Capture-Portfolio',
+    visit: 'https://capture-portfolio-two.vercel.app/',
+    id: 5,
+  },
+  {
+    title: 'Countdown',
+    description:
+      'A nice little JavaScript Project that counts down to something the user entered. It does all the remaining time calculations until the countdown is complete. This project was purely built with vanilla JavaScript and HTML.',
+    image: '/images/Countdown.png',
+    tags: ['Javascript', 'HTML', 'CSS'],
+    source: 'https://github.com/NesyElimelech/Final-Countdown',
+    visit: 'https://final-countdown-one.vercel.app/',
+    id: 6,
+  },
+  {
+    title: 'Disney Plus Clone',
+    description:
+      "This is a Disney Plus's website clone, Here I've use React Context API and Styled Components for the Front-End, And Firebase's realtime database and authentication for the Back-End. It's built with 3 pages: Welcome page, Home page and a Movie/Series page. all the data is fetched from the firebase database using async/await data fetching.",
+    image: '/images/DisneyPlus.png',
+    tags: ['React', 'Styled-Components', 'Firebase'],
+    source: 'https://github.com/NesyElimelech/disney_plus_clone',
+    visit: 'https://disney-plus-clone-hazel.vercel.app/home',
     id: 7,
   },
   {
     title: 'Beats Landing page example',
     description:
-      "This is a sample of a responsive landing page for an headphone company. some of it's features and a links to shop the headphones. The page has scroll animations with 3ed party library 'scrollreveal.js'",
+      "This is a example of a responsive landing page for an headphone company. some of it's features and a links to shop the headphones. The page has scroll animations with 3ed party library 'scrollreveal.js'",
     image: '/images/headphone.png',
     tags: ['HTML', 'CSS', 'JavaScript'],
     source: 'https://github.com/NesyElimelech/headphone-landing-page',
